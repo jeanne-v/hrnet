ed811f2e7d392578bcc65f8661c38762283d9b49
Jeanne Viallard <viallard.jeanne@gmail.com>
2026-03-18 11:44:12 +0100

    Add table styles + basic sort

--- src/pages/CurrentEmployees/index.jsx
+++ src/pages/CurrentEmployees/index.jsx
@@ -1,44 +1,110 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import styles from "./CurrentEmployees.module.css";
 
+import sortIcon from "../../assets/sort.svg";
+import sortUpIcon from "../../assets/sort-up.svg";
+import sortDownIcon from "../../assets/sort-down.svg";
+
+function getSortedRows(rows, sortObj) {
+  if (!sortObj) {
+    return rows;
+  }
+
+  return rows.toSorted((a, b) => {
+    const valueA = a[sortObj.value];
+    const valueB = b[sortObj.value];
+    if (sortObj.type === "descending") {
+      return valueB.localeCompare(valueA);
+    } else {
+      return valueA.localeCompare(valueB);
+    }
+  });
+}
+
 export default function CurrentEmployees() {
+  const [currentSort, setCurrentSort] = useState(null);
   const employees = useSelector((state) => state.employees);
+  const headers = [
+    { text: "First Name", value: "firstName" },
+    { text: "Last Name", value: "lastName" },
+    { text: "Start Date", value: "startDate" },
+    { text: "Department", value: "department" },
+    { text: "Date of Birth", value: "dateOfBirth" },
+    { text: "Street", value: "street" },
+    { text: "City", value: "city" },
+    { text: "State", value: "state" },
+    { text: "Zip Code", value: "zipCode" },
+  ];
+
+  let rows = getSortedRows(employees, currentSort);
+
+  function onSortBtnClick(e) {
+    const value = e.currentTarget.dataset.value;
+    if (!currentSort || currentSort.value !== value) {
+      setCurrentSort({ value, type: "ascending" });
+      return;
+    }
+    if (currentSort.type === "ascending") {
+      setCurrentSort((prevSort) => ({ ...prevSort, type: "descending" }));
+    } else {
+      setCurrentSort(null);
+    }
+  }
+
   return (
     <div>
       <h1 className={styles.heading}>Current Employees</h1>
       <div className={styles.content}>
-        <table className={styles.table}>
-          <thead>
-            <tr>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Start Date</th>
-              <th>Department</th>
-              <th>Date of Birth</th>
-              <th>Street</th>
-              <th>City</th>
-              <th>State</th>
-              <th>Zip Code</th>
-            </tr>
-          </thead>
-          <tbody>
-            {employees.map((employee) => {
-              return (
-                <tr>
-                  <td>{employee.firstName}</td>
-                  <td>{employee.lastName}</td>
-                  <td>{employee.startDate}</td>
-                  <td>{employee.department}</td>
-                  <td>{employee.dateOfBirth}</td>
-                  <td>{employee.street}</td>
-                  <td>{employee.city}</td>
-                  <td>{employee.state}</td>
-                  <td>{employee.zipCode}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <div className={styles.container}>
+          <table className={styles.table}>
+            <thead className={styles.headers}>
+              <tr>
+                {headers.map((header) => {
+                  let icon = sortIcon;
+                  const isCurrentSort = currentSort && currentSort.value === header.value;
+                  if (isCurrentSort && currentSort.type === "ascending")
+                    icon = sortUpIcon;
+                  if (isCurrentSort && currentSort.type === "descending")
+                    icon = sortDownIcon;
+
+                  return (
+                    <th
+                      key={header.value}
+                      aria-sort={isCurrentSort ? currentSort.type : null}
+                    >
+                      <button
+                        className={styles.sort}
+                        data-value={header.value}
+                        onClick={onSortBtnClick}
+                      >
+                        {header.text}
+                        <img src={icon} alt="" className={styles.icon} />
+                      </button>
+                    </th>
+                  );
+                })}
+              </tr>
+            </thead>
+            <tbody className={styles.rows}>
+              {rows.map((employee, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{employee.firstName}</td>
+                    <td>{employee.lastName}</td>
+                    <td>{employee.startDate}</td>
+                    <td>{employee.department}</td>
+                    <td>{employee.dateOfBirth}</td>
+                    <td>{employee.street}</td>
+                    <td>{employee.city}</td>
+                    <td>{employee.state}</td>
+                    <td>{employee.zipCode}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
