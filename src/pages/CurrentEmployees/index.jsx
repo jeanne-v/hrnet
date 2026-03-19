6e3e3a944682b7653b9e1959ad93657f6e8eaf56
Jeanne Viallard <viallard.jeanne@gmail.com>
2026-03-19 14:03:05 +0100

    Add pagination

--- src/pages/CurrentEmployees/index.jsx
+++ src/pages/CurrentEmployees/index.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
+import { MenuSelect } from "menu-select";
 import styles from "./CurrentEmployees.module.css";
 
 import sortIcon from "../../assets/sort.svg";
 import sortUpIcon from "../../assets/sort-up.svg";
 import sortDownIcon from "../../assets/sort-down.svg";
+import chevronLeftIcon from "../../assets/chevron-left.svg";
+import chevronRightIcon from "../../assets/chevron-right.svg";
 
 function getSearchFilteredRows(rows, searchFilter) {
   if (!searchFilter) {
@@ -38,6 +41,16 @@ function getSortedRows(rows, sortObj) {
 export default function CurrentEmployees() {
   const [searchInputValue, setSearchInputValue] = useState("");
   const [currentSort, setCurrentSort] = useState(null);
+
+  const numbersOptions = [
+    { text: "10", value: "10" },
+    { text: "25", value: "25" },
+    { text: "50", value: "50" },
+    { text: "100", value: "100" },
+  ];
+  const [selectedNumberOption, setSelectedNumberOption] = useState(numbersOptions[0]);
+  const [currentPage, setCurrentPage] = useState(1);
+
   const employees = useSelector((state) => state.employees);
   const headers = [
     { text: "First Name", value: "firstName" },
@@ -51,9 +64,16 @@ export default function CurrentEmployees() {
     { text: "Zip Code", value: "zipCode" },
   ];
 
-  let rows = getSearchFilteredRows(employees, searchInputValue);
+  const maxNumberOfRows = +selectedNumberOption.value;
 
-  rows = getSortedRows(rows, currentSort);
+  const filteredRows = getSearchFilteredRows(employees, searchInputValue);
+
+  const sortedRows = getSortedRows(filteredRows, currentSort);
+
+  const startIndex = maxNumberOfRows * (currentPage - 1);
+  const endIndex = maxNumberOfRows * currentPage - 1;
+
+  const visibleRows = sortedRows.slice(startIndex, endIndex + 1);
 
   function onSortBtnClick(e) {
     const value = e.currentTarget.dataset.value;
@@ -72,11 +92,31 @@ export default function CurrentEmployees() {
     setSearchInputValue(e.target.value);
   }
 
+  function handleNextPageBtnClick() {
+    setCurrentPage((prevValue) => prevValue + 1);
+  }
+
+  function handlePreviousPageBtnClick() {
+    setCurrentPage((prevValue) => prevValue - 1);
+  }
+
   return (
     <div>
       <h1 className={styles.heading}>Current Employees</h1>
       <div className={styles.content}>
         <div className={styles.filter}>
+          <div className={styles.limit}>
+            <p id="number-of-rows">Show rows</p>
+            <div>
+              <MenuSelect
+                labelledby="number-of-rows"
+                options={numbersOptions}
+                selectedOption={selectedNumberOption}
+                setSelectedOption={setSelectedNumberOption}
+                size="small"
+              />
+            </div>
+          </div>
           <div className={styles.search}>
             <label htmlFor="search-input">Search:</label>
             <input
@@ -120,7 +160,7 @@ export default function CurrentEmployees() {
               </tr>
             </thead>
             <tbody className={styles.rows}>
-              {rows.map((employee, index) => {
+              {visibleRows.map((employee, index) => {
                 return (
                   <tr key={index}>
                     <td>{employee.firstName}</td>
@@ -137,6 +177,33 @@ export default function CurrentEmployees() {
               })}
             </tbody>
           </table>
+        </div>
+        <div className={styles["pagination-container"]}>
+          <p className={styles["rows-nb"]}>
+            Showing {startIndex + 1} to{" "}
+            {filteredRows.length > endIndex + 1 ? endIndex + 1 : filteredRows.length} of{" "}
+            {filteredRows.length} entries{" "}
+            {filteredRows.length === employees.length
+              ? ""
+              : `(filtered from total ${employees.length} entries)`}
+          </p>
+          <div className={styles.pagination}>
+            <button
+              className={styles["pagination-btn"]}
+              disabled={currentPage === 1}
+              onClick={handlePreviousPageBtnClick}
+            >
+              <img src={chevronLeftIcon} alt="Previous page" />
+            </button>
+            <p className={styles["pagination-text"]}>{currentPage}</p>
+            <button
+              className={styles["pagination-btn"]}
+              disabled={filteredRows.length <= endIndex + 1}
+              onClick={handleNextPageBtnClick}
+            >
+              <img src={chevronRightIcon} alt="Next page" />
+            </button>
+          </div>
         </div>
       </div>
     </div>
